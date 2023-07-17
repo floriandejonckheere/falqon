12ea1240c582f28af7948c887f4e476d6b6a3ff8
Florian Dejonckheere <florian@floriandejonckheere.be>
2023-07-17 19:19:04 +0200

    Increase payload size

--- examples/worker/app.rb
+++ examples/worker/app.rb
@@ -21,8 +21,24 @@ i = 0
 loop do
   puts "Enqueueing job #{i}..."
 
-  data = { id: i, time: Time.now.to_i, sleep: rand(0.4..0.5) }
-  data[:long] = "x" * 1024 if rand(10).zero?
+  data = {
+    id: i,
+    time: Time.now.to_i,
+    sleep: rand(0.4..0.5),
+    data: {
+      user: {
+        id: rand(1000),
+        name: "User #{rand(1000)}",
+        email: "user#{rand(1000)}@localhost",
+      },
+      message: "Hello, w#{rand(10).zero? ? 'o' * 1000 : 'o'}rld!",
+      system: {
+        pid: Process.pid,
+        hostname: Socket.gethostname,
+        uptime: `uptime`.chomp,
+      },
+    },
+  }
 
   queue.push(data.to_json)
 
