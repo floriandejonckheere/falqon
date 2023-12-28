e796df50eecd61b53f22f73b438ed1b57e66c8d5
Florian Dejonckheere <florian@floriandejonckheere.be>
2023-12-28 19:32:51 +0100

    Add connection pool snooper

---
+++ lib/falqon/connection_pool_snooper.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module Falqon
+  ##
+  # Connection pool that logs method calls
+  #
+  class ConnectionPoolSnooper < ConnectionPool
+    def with(...)
+      puts "#{caller(1..1).first[/.*:in/][0..-4]} #{caller(1..1).first[/`.*'/][1..-2]}"
+
+      super(...)
+    end
+  end
+end
