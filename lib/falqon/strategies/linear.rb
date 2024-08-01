e7d0854389c348d0f9947de544b01de720d7efd5
Florian Dejonckheere <florian@floriandejonckheere.be>
2024-08-01 19:11:30 +0200

    Adjust linear strategy to reschedule entries

--- lib/falqon/strategies/linear.rb
+++ lib/falqon/strategies/linear.rb
@@ -16,13 +16,23 @@ module Falqon
 
           r.multi do |t|
             if retries < queue.max_retries || queue.max_retries == -1
-              queue.logger.debug "Requeuing message #{message.id} on queue #{queue.name} (attempt #{retries})"
+              if queue.retry_delay.positive?
+                queue.logger.debug "Scheduling message #{message.id} on queue #{queue.name} in #{queue.retry_delay} seconds (attempt #{retries})"
 
-              # Add identifier back to pending queue
-              queue.pending.add(message.id)
+                # Add identifier to scheduled queue
+                queue.scheduled.add(message.id, Time.now.to_i + queue.retry_delay)
 
-              # Set message status
-              t.hset("#{queue.id}:metadata:#{message.id}", :status, "pending")
+                # Set message status
+                t.hset("#{queue.id}:metadata:#{message.id}", :status, "scheduled")
+              else
+                queue.logger.debug "Requeuing message #{message.id} on queue #{queue.name} (attempt #{retries})"
+
+                # Add identifier back to pending queue
+                queue.pending.add(message.id)
+
+                # Set message status
+                t.hset("#{queue.id}:metadata:#{message.id}", :status, "pending")
+              end
             else
               # Kill message after max retries
               message.kill
--- spec/falqon/strategies/linear_spec.rb
+++ spec/falqon/strategies/linear_spec.rb
@@ -1,66 +1,153 @@
 # frozen_string_literal: true
 
 RSpec.describe Falqon::Strategies::Linear do
-  subject(:queue) { Falqon::Queue.new("name", retry_strategy: :linear) }
+  subject(:queue) { Falqon::Queue.new("name", retry_strategy: :linear, retry_delay:) }
 
   describe "#retry" do
-    it "requeues messages if they fail" do
-      queue.push("message1", "message2")
+    context "when retry delay is zero" do
+      let(:retry_delay) { 0 }
 
-      queue.pop { raise Falqon::Error }
-
-      queue.redis.with do |r|
-        expect(r.lrange("falqon/name", 0, -1)).to eq ["2", "1"]
-
-        expect(r.hget("falqon/name:metadata:1", :retries)).to eq "1"
-      end
-    end
-
-    it "sets the message status to pending" do
-      id = queue.push("message1")
-
-      queue.pop { raise Falqon::Error }
-
-      message = Falqon::Message.new(queue, id:)
-
-      expect(message.metadata.status).to eq "pending"
-    end
-
-    context "when processing fails too many times" do
-      it "kills messages" do
-        queue.push("message1")
+      it "requeues messages immediately" do
+        queue.push("message1", "message2")
 
         queue.pop { raise Falqon::Error }
-        queue.pop { raise Falqon::Error }
-        queue.pop { raise Falqon::Error }
 
-        expect(queue.pending).to be_empty
-        expect(queue.processing).to be_empty
-        expect(queue.dead).not_to be_empty
+        queue.redis.with do |r|
+          expect(r.lrange("falqon/name", 0, -1)).to eq ["2", "1"]
+
+          expect(r.hget("falqon/name:metadata:1", :retries)).to eq "1"
+        end
       end
 
-      it "resets the retry counter" do
+      it "sets the message status to pending" do
         id = queue.push("message1")
 
-        queue.pop { raise Falqon::Error }
-        queue.pop { raise Falqon::Error }
         queue.pop { raise Falqon::Error }
 
         message = Falqon::Message.new(queue, id:)
 
-        expect(message.metadata.retries).to be_zero
+        expect(message.metadata.status).to eq "pending"
       end
 
-      it "sets the message status to dead" do
-        id = queue.push("message1")
+      context "when processing fails too many times" do
+        it "kills messages" do
+          queue.push("message1")
+
+          queue.pop { raise Falqon::Error }
+          queue.pop { raise Falqon::Error }
+          queue.pop { raise Falqon::Error }
+
+          expect(queue.pending).to be_empty
+          expect(queue.processing).to be_empty
+          expect(queue.dead).not_to be_empty
+        end
+
+        it "resets the retry counter" do
+          id = queue.push("message1")
+
+          queue.pop { raise Falqon::Error }
+          queue.pop { raise Falqon::Error }
+          queue.pop { raise Falqon::Error }
+
+          message = Falqon::Message.new(queue, id:)
+
+          expect(message.metadata.retries).to be_zero
+        end
+
+        it "sets the message status to dead" do
+          id = queue.push("message1")
+
+          queue.pop { raise Falqon::Error }
+          queue.pop { raise Falqon::Error }
+          queue.pop { raise Falqon::Error }
+
+          message = Falqon::Message.new(queue, id:)
+
+          expect(message.metadata.status).to eq "dead"
+        end
+      end
+    end
+
+    context "when retry delay is greater than zero" do
+      let(:retry_delay) { 5 }
+
+      it "reschedules messages" do
+        queue.push("message1", "message2")
 
         queue.pop { raise Falqon::Error }
-        queue.pop { raise Falqon::Error }
+
+        queue.redis.with do |r|
+          expect(r.lrange("falqon/name", 0, -1)).to eq ["2"]
+          expect(r.zrange("falqon/name:scheduled", 0, -1)).to eq ["1"]
+
+          expect(r.hget("falqon/name:metadata:1", :retries)).to eq "1"
+        end
+      end
+
+      it "sets the message status to scheduled" do
+        id = queue.push("message1")
+
         queue.pop { raise Falqon::Error }
 
         message = Falqon::Message.new(queue, id:)
 
-        expect(message.metadata.status).to eq "dead"
+        expect(message.metadata.status).to eq "scheduled"
+      end
+
+      context "when processing fails too many times" do
+        it "kills messages" do
+          queue.push("message1")
+
+          queue.pop { raise Falqon::Error }
+
+          Timecop.travel(queue.retry_delay) { queue.schedule }
+
+          queue.pop { raise Falqon::Error }
+
+          Timecop.travel(queue.retry_delay) { queue.schedule }
+
+          queue.pop { raise Falqon::Error }
+
+          expect(queue.pending).to be_empty
+          expect(queue.processing).to be_empty
+          expect(queue.dead).not_to be_empty
+        end
+
+        it "resets the retry counter" do
+          id = queue.push("message1")
+
+          queue.pop { raise Falqon::Error }
+
+          Timecop.travel(queue.retry_delay) { queue.schedule }
+
+          queue.pop { raise Falqon::Error }
+
+          Timecop.travel(queue.retry_delay) { queue.schedule }
+
+          queue.pop { raise Falqon::Error }
+
+          message = Falqon::Message.new(queue, id:)
+
+          expect(message.metadata.retries).to be_zero
+        end
+
+        it "sets the message status to dead" do
+          id = queue.push("message1")
+
+          queue.pop { raise Falqon::Error }
+
+          Timecop.travel(queue.retry_delay) { queue.schedule }
+
+          queue.pop { raise Falqon::Error }
+
+          Timecop.travel(queue.retry_delay) { queue.schedule }
+
+          queue.pop { raise Falqon::Error }
+
+          message = Falqon::Message.new(queue, id:)
+
+          expect(message.metadata.status).to eq "dead"
+        end
       end
     end
   end
